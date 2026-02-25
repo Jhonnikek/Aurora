4dc7f415c8145aaabaaa3fdc6b623ffbdbee03bd
Jhonnikek <138178283+Jhonnikek@users.noreply.github.com>
2026-02-25 18:18:51 -0500

    chore: mode meme function to fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,11 +1,30 @@
-from __future__ import annotations
+import aiohttp
 from discord.ext import commands
+import logging
 import random
 
 
 class Fun(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
+        self.session = aiohttp.ClientSession()
+
+    async def cog_unload(self):
+        await self.session.close()
+
+    @commands.command()
+    async def meme(self, ctx):
+        try:
+            async with self.session.get("https://meme-api.com/gimme") as response:
+                if response.status == 200:
+                    meme_data = await response.json()
+                    await ctx.send(meme_data["url"])
+                else:
+                    logging.warning(
+                        f"Meme API request failed with status: {response.status}"
+                    )
+        except aiohttp.ClientError as e:
+            logging.error(f"Error fetching meme: {e}")
 
     @commands.command()
     async def ping(self, ctx) -> None:
