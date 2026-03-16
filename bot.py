495b452f062652545d9e0330abb590dbd2cc5c48
Jhonnikek <138178283+Jhonnikek@users.noreply.github.com>
2026-03-16 11:19:28 -0500

    update bot.py

--- bot.py
+++ bot.py
@@ -8,19 +8,34 @@ load_dotenv()
 
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 
+
 class Aurora(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.message_content = True
-        activity = discord.Game(name='not being in epstein files')
-        super().__init__(intents=intents, command_prefix=commands.when_mentioned_or("!"), activity=activity, help_command=None)
+        activity = discord.Game(name="Not in epstein files")
+        status = discord.Status.do_not_disturb
+        super().__init__(
+            intents=intents,
+            command_prefix=commands.when_mentioned_or("!"),
+            activity=activity,
+            status=status,
+            help_command=None,
+        )
 
     async def on_ready(self):
         print(f"{self.user.name} is online")
 
+    async def load_cogs(self) -> None:
+        for file in os.listdir("./cogs"):
+            if file.endswith(".py"):
+                extension = file.rstrip(".py")
+                await self.load_extension(f"cogs.{extension}")
+
     async def setup_hook(self) -> None:
-        await self.load_extension('cogs.memes')
+        await self.load_cogs()
         return await super().setup_hook()
+
 
 if __name__ == "__main__":
     bot = Aurora()
